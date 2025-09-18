6280be74e844dd6a3deb8181ef292d0d6adffed4
zainab-nooh <zain3b.nooh@gmail.com>
2025-09-18 03:54:07 +0300

    add main screens to the app

---
+++ AlosraRecipez/src/screens/main/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { globalStyles } from '../../styles/globalStyles';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={[globalStyles.container, globalStyles.centerContainer]}>
+      <Text style={globalStyles.title}>Home Screen</Text>
+      <Text style={globalStyles.caption}>To be implemented with complete app</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
